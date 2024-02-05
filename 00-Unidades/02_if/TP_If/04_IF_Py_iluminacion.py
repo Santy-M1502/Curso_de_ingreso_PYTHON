0a69dd244513c7c0f40210be58014b749e11aa99
Santiago <santy15021502@gmail.com>
2024-02-05 11:55:49 -0300

    2 Carpetas listo

--- 00-Unidades/02_if/TP_If/04_IF_Py_iluminacion.py
+++ 00-Unidades/02_if/TP_If/04_IF_Py_iluminacion.py
@@ -5,12 +5,6 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
----
-TP: IF_Iluminacion
----
-Enunciado:
 Todas las lámparas están  al mismo precio de $800 pesos final.
 		A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 		B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
@@ -24,6 +18,7 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__() 
 
+        # configure window
         self.title("UTN Fra")
 
         self.label1 = customtkinter.CTkLabel(master=self, text="Marca")
@@ -43,7 +38,43 @@ class App(customtkinter.CTk):
 
 
     def btn_calcular_on_click(self):
-        pass
+        #Valores
+        precio_lampara = 800
+        cantidad_lamparas_texto = self.combobox_cantidad.get()
+        cantidad_lamparas_numero = int(cantidad_lamparas_texto)
+        marca_lampara = self.combobox_marca.get()
+        precio_lampara = precio_lampara * cantidad_lamparas_numero
+        #Calculos
+        if(cantidad_lamparas_numero >= 6):
+            precio_lampara = precio_lampara / 2
+        if(cantidad_lamparas_numero == 5 and marca_lampara == "ArgentinaLuz"):
+            descuento_40 = 40 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_40
+        elif(cantidad_lamparas_numero == 5):
+            descuento_30 = 30 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_30
+        if(cantidad_lamparas_numero == 4 and marca_lampara == "ArgentinaLuz"):
+            descuento_25 = 25 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_25
+        elif(cantidad_lamparas_numero == 4 and marca_lampara == "FelipeLamparas"):
+            descuento_25 = 25 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_25
+        elif(cantidad_lamparas_numero == 4):
+            descuento_20 = 20 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_20
+        if(cantidad_lamparas_numero == 3 and marca_lampara == "ArgentinaLuz"):
+            descuento_15 = 15 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_15
+        elif(cantidad_lamparas_numero == 3 and marca_lampara == "FelipeLamparas"):
+            descuento_10 = 10 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_10
+        elif(cantidad_lamparas_numero == 3):
+            descuento_5 = 5 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_5
+        if(precio_lampara > 4000):
+            descuento_5 = 5 * precio_lampara / 100
+            precio_lampara = precio_lampara - descuento_5
+        alert("LamparasOferton", "El precio de la/s lampara/s es: " + str(precio_lampara))
         
     
 if __name__ == "__main__":
